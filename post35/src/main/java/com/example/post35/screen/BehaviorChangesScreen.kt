458b1c4f22b7574dc58cc137206f2566bf29e1e6
George <ridiculousman@yandex.ru>
2025-03-01 02:47:51 +0400

    Dnd for post35

--- post35/src/main/java/com/example/post35/screen/BehaviorChangesScreen.kt
+++ post35/src/main/java/com/example/post35/screen/BehaviorChangesScreen.kt
@@ -1,6 +1,14 @@
 package com.example.post35.screen
 
+import android.app.NotificationManager
+import android.app.NotificationManager.INTERRUPTION_FILTER_ALL
+import android.app.NotificationManager.INTERRUPTION_FILTER_NONE
+import android.content.Context
+import android.content.Intent
 import android.os.Build
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.post35.components.AppBar
@@ -20,6 +29,16 @@ import com.example.post35.service.NotificationHelper
 
 @Composable
 fun BehaviorChangesScreen(notificationHelper: NotificationHelper) {
+    val context = LocalContext.current
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE)
+            as NotificationManager
+
+    val activityLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) {
+        toggleDND(notificationManager)
+    }
+
     Scaffold(
         modifier = Modifier.safeDrawingPadding(),
         topBar = { AppBar(name = stringResource(R.string.label_behavior_changes)) }
@@ -51,6 +70,29 @@ fun BehaviorChangesScreen(notificationHelper: NotificationHelper) {
             ) {
                 Text(stringResource(R.string.bc_send_notification))
             }
+
+            Text(stringResource(R.string.bc_toggle_dnd_hint))
+
+
+            Button(
+                onClick = {
+                    if (!notificationManager.isNotificationPolicyAccessGranted) {
+                        val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                        activityLauncher.launch(intent)
+                    } else toggleDND(notificationManager)
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.bc_toggle_dnd))
+            }
         }
     }
+}
+
+fun toggleDND(notificationManager: NotificationManager) {
+    notificationManager.setInterruptionFilter(
+        if (notificationManager.currentInterruptionFilter == INTERRUPTION_FILTER_NONE)
+            INTERRUPTION_FILTER_ALL
+        else INTERRUPTION_FILTER_NONE
+    )
 }
